00990849e790b0f48cef216bf28462aa1d99f861
Rasmus Kjær Mortensen <s185466@student.dtu.dk>
2018-11-16 09:05:09 +0100

    commit test

--- 35_del3/src/Game.java
+++ 35_del3/src/Game.java
@@ -50,7 +50,7 @@ public class Game {
                 System.out.println((String) jText.get("sq3"));
                 break;
             case 4:
-                playerOne.balance.addPoints(100);
+                playerOne.balance.addPoints(100);gfyfghdhj
                 System.out.println((String) jText.get("sq4"));
                 break;
             case 5:
