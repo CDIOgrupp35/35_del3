5cba3bcbff7d7ae4ba0e7c5791bbb74ba59b0ac6
Siff <siff.ravn@gmail.com>
2018-11-20 11:38:51 +0100

    UneventfulSq class implemented

--- 35_del3/src/Squares/UneventfulSq.java
+++ 35_del3/src/Squares/UneventfulSq.java
@@ -2,4 +2,12 @@ package Squares;
 
 
 public class UneventfulSq extends Square {
+
+    public UneventfulSq(int sqNum, String sqName){
+        super(sqNum, sqName);
+    }
+
+    public void landOn(Player player){
+        System.out.println("Take a break");
+    }
 }
