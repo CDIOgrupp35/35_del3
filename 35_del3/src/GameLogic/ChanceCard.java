dc75e592f737ffa2371c19b83b064e9cbb023ccd
Gustav Vang Klinker <gulle139@hotmail.com>
2018-11-22 20:01:06 +0100

    Implement chancekort

---
+++ 35_del3/src/GameLogic/ChanceCard.java
@@ -0,0 +1,29 @@
+package GameLogic;
+import Player.*;
+
+public class ChanceCard {
+    Player player;
+    GameRules gRule;
+    String cardName;
+    String CardText;
+    int cardEffect;
+
+    ChanceCard(String cardName, String cardText, int cardEffect){
+        this.cardName = cardName;
+        this.CardText = cardText;
+        this.cardEffect = cardEffect;
+    }
+
+    public void setCardEffect(){
+        switch (cardEffect) {
+            case 1:
+                gRule.passStart(player, 0);
+                break;
+            case 2:
+                gRule.movePlayer(player, 5);
+                break;
+            case 3:
+
+        }
+    }
+}
