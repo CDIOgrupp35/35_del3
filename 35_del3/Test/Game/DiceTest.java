0b13ff8160a8882f713af23f6b598c767a8c8e46
Tim <s185027@student.dtu.dk>
2018-11-29 18:28:18 +0100

    DiceTest, testing fairness of roll.

--- 35_del3/Test/Game/DiceTest.java
+++ 35_del3/Test/Game/DiceTest.java
@@ -6,8 +6,8 @@ import static org.junit.Assert.*;
 
 class DiceTest {
 
-    private Dice diceTest = new Dice(1,6);
-    private Dice diceTest2 = new Dice(2,10);
+    private Dice diceTest1 = new Dice(1, 6);
+    private Dice diceTest2 = new Dice(2, 10);
 
 
     /**
@@ -15,10 +15,10 @@ class DiceTest {
      */
     @RepeatedTest(500)
     void rollSidesTest() {
-        int[] diceArray = diceTest.roll();
+        int[] diceArray = diceTest1.roll();
 
         int minRoll = 1;
-        int maxRoll = diceTest.getSides();
+        int maxRoll = diceTest1.getSides();
 
         for (int i = 0; i < diceArray.length; i++) {
             assertTrue(diceArray[i] >= minRoll && diceArray[i] <= maxRoll);
@@ -43,14 +43,61 @@ class DiceTest {
     @Test
     void sumTest() throws Exception {
 
-        diceTest2.setFaceValue(0,5);
-        diceTest2.setFaceValue(1,6);
+        diceTest2.setFaceValue(0, 5);
+        diceTest2.setFaceValue(1, 6);
 
         assertEquals(diceTest2.sum(), 11);
 
     }
 
     /**
-     * Testing that the roll-method is fair.
+     * Testing that the roll-method is fair, with a 4% margin of error and 60000 rolls.
      */
+    @Test
+    void testFairness() {
+        int ones, twos, threes, fours, fives, sixes;
+        ones = twos = threes = fours = fives = sixes = 0;
+
+        int lowerBound = 9600;
+        int upperBound = 10400;
+
+        for (int i = 0; i < 60000; i++) {
+            diceTest1.roll();
+
+            int rollResult = diceTest1.getFaceValue(0); //rollResult is faceValue of diceArray index 0, since diceTest1 only has 1 dice.
+
+            switch(rollResult) {
+                case 1:
+                    ones++;
+                    break;
+                case 2:
+                    twos++;
+                    break;
+                case 3:
+                    threes++;
+                    break;
+                case 4:
+                    fours++;
+                    break;
+                case 5:
+                    fives++;
+                    break;
+                case 6:
+                    sixes++;
+                    break;
+                default:
+                    fail();
+                    break;
+            }
+
+        }
+
+        assertTrue((ones >= lowerBound) && (ones <= upperBound));
+        assertTrue((twos >= lowerBound) && (twos <= upperBound));
+        assertTrue((threes >= lowerBound) && (threes <= upperBound));
+        assertTrue((fours >= lowerBound) && (fours <= upperBound));
+        assertTrue((fives >= lowerBound) && (fives <= upperBound));
+        assertTrue((sixes >= lowerBound) && (sixes <= upperBound));
+
+    }
 }
