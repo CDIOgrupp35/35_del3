9bc8061cdb86c6237329f7e201ac5ff692012ea1
Tim <s185027@student.dtu.dk>
2018-11-30 10:34:31 +0100

    Created ChanceCardTest

---
+++ 35_del3/Test/Game/ChanceCardTest.java
@@ -0,0 +1,16 @@
+package Game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChanceCardTest {
+
+    @Test
+    void executeEffect() {
+    }
+
+    @Test
+    void getCardText() {
+    }
+}
