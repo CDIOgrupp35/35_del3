5de341c8884f0df16cfdb138aee7d03cb13b5683
Gustav Vang Klinker <gulle139@hotmail.com>
2018-11-30 11:02:03 +0100

    Fixed sqMessages

--- 35_del3/src/Game/Squares/UneventfulSq.java
+++ 35_del3/src/Game/Squares/UneventfulSq.java
@@ -9,8 +9,9 @@ public class UneventfulSq extends Square {
 
     public void landOn(Player player){
         super.landOn(player);
-        if (!(sqName.equalsIgnoreCase("Start"))) {
+        if (!(sqName.equalsIgnoreCase("Start")) && !landedOn) {
             setSqMessage(getSqMessage() +  ". Din tur er slut.");
+            landedOn = true;
         }
     }
 }
